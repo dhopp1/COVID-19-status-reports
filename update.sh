30e472681f54aad70ff1dfc7542b91b9366f0c55
dhopp1 <daniel.hopp1@gmail.com>
2020-03-30 15:58:13 +0200

    Changenge layout to add all countries

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 git -C COVID-19 pull &&
-jupyter nbconvert --to notebook --execute COVID-19\ Status\ Report.ipynb --output COVID-19\ Status\ Report.ipynb &&
+jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute COVID-19\ Status\ Report.ipynb --output COVID-19\ Status\ Report.ipynb &&
 jupyter nbconvert COVID-19\ Status\ Report.ipynb &&
 mv COVID-19\ Status\ Report.html index.html
