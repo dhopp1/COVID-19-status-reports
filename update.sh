780d23b23e6115bc56e0fe8eb1e3227ab89b637b
dhopp1 <daniel.hopp1@gmail.com>
2020-03-30 20:59:06 +0200

    Fix bug with death acceleration, add git push to update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,7 @@
 git -C COVID-19 pull &&
 jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute COVID-19\ Status\ Report.ipynb --output COVID-19\ Status\ Report.ipynb &&
 jupyter nbconvert COVID-19\ Status\ Report.ipynb &&
-mv COVID-19\ Status\ Report.html index.html
+mv COVID-19\ Status\ Report.html index.html &&
+git add . &&
+git commit -m "Daily update" &&
+git push origin master
