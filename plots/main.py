59a90d6be44295270a9d864d6a790d07f044b4e2
dhopp1 <daniel.hopp1@gmail.com>
2020-04-04 17:29:08 +0200

    Add more bokeh plots, set up filter logic

--- plots/main.py
+++ plots/main.py
@@ -2,52 +2,150 @@ from bokeh.plotting import figure
 from bokeh.plotting import ColumnDataSource, curdoc
 from bokeh.io import output_file, show
 from bokeh.layouts import column, row
-from bokeh.models import DatetimeTickFormatter, HoverTool, Select
+from bokeh.models import DatetimeTickFormatter, HoverTool, NumeralTickFormatter, Select, Span
 import pandas as pd
 
 # data read
-data = pd.read_csv("data/transformed_data.csv")
-data.date = pd.to_datetime(data.date)
-forecasts = pd.read_csv("data/forecasts.csv")
-forecasts.date = pd.to_datetime(forecasts.date)
+data = pd.read_csv("plots/data/transformed_data.csv", parse_dates=["date"])
+data["date_string"] = data.date.dt.strftime("%Y-%0m-%0d")
+data["x_col"] = data.date
+forecasts = pd.read_csv("plots/data/forecasts.csv", parse_dates=["date"])
+forecasts["date_string"] = forecasts.date.dt.strftime("%Y-%0m-%0d")
+forecasts["x_col"] = forecasts.date
+countries = list(data.country.unique())
+countries.sort()
+countries.remove("World")
+countries = ["World", "None"] + countries
+dates = list(data.date.dt.strftime("%Y-%0m-%0d").unique())
+dates.sort()
 
-source = ColumnDataSource(data.loc[data.country == "Italy", :])
-source2 = ColumnDataSource(data.loc[data.country == "US", :])
+source = ColumnDataSource(data.loc[data.country == "World", :])
+source2 = ColumnDataSource(data.loc[data.country == "None", :])
+
 
 # defining plots
-def line_plot(source, p, color, country):
+def line_plot(source, p, color, country, metric):
     """data = ColumnDataSource, country = country name, p = bokeh figure"""
-    
-    p.left[0].formatter.use_scientific = False
-    p.line('date', 'confirmed', source=source, color=color)
-    p.xaxis.formatter=DatetimeTickFormatter(
-            days=["%d %B %Y"],
-            months=["%d %B %Y"],
-            years=["%d %B %Y"],
-        )
+    p.line('x_col', metric, source=source, color=color)
+    return p
+
+def add_plot(plot_function, metric, title):
+    p = figure(tools=["reset", "pan", "zoom_in", "zoom_out","save"], title=title)
+    plot_function(source, p, "#21618C", select1.value, metric)
+    plot_function(source2, p, "#ff4d4d", select1.value, metric)
+    p.add_tools(
+        HoverTool(tooltips=[('Country', '@country'), ("Date", "@date_string"), (title,f'@{metric}')])
+    )
     p.xaxis.major_label_orientation = 3.14/4
+    p.renderers.extend([Span(location=0, dimension='width', line_color='black', line_width=1)]) # adding a horizontal black line at 0
+    p.left[0].formatter.use_scientific = False
     return p
 
 
+# selector/dropdown functions
 def country_1_update_plot(attr, old, new):
-    source.data = data.loc[data.country == new, :].reset_index(drop=True)
+    source.data = data.loc[
+        (data.date >= pd.to_datetime(select3.value)) & 
+        (data.date <= pd.to_datetime(select4.value)) &
+        (data.country == new)
+    , :].reset_index(drop=True)
     
 def country_2_update_plot(attr, old, new):
-    source2.data = data.loc[data.country == new, :].reset_index(drop=True)
+    source2.data = data.loc[
+        (data.date >= pd.to_datetime(select3.value)) & 
+        (data.date <= pd.to_datetime(select4.value)) &
+        (data.country == new)
+    , :].reset_index(drop=True)
 
-select1 = Select(title="Country 1", options=["Italy", "US"], value="Italy")
+def date_start_update_plot(attr, old, new):
+    source.data = data.loc[
+        (data.date >= pd.to_datetime(new)) & 
+        (data.date <= pd.to_datetime(select4.value)) &
+        (data.country == select1.value)
+    , :].reset_index(drop=True)
+    source2.data = data.loc[
+        (data.date >= pd.to_datetime(new)) & 
+        (data.date <= pd.to_datetime(select4.value)) &
+        (data.country == select2.value)
+    , :].reset_index(drop=True)
+
+def date_end_update_plot(attr, old, new):
+    source.data = data.loc[
+        (data.date >= pd.to_datetime(select3.value)) & 
+        (data.date <= pd.to_datetime(new)) &
+        (data.country == select1.value)
+    , :].reset_index(drop=True)
+    source2.data = data.loc[
+        (data.date >= pd.to_datetime(select3.value)) & 
+        (data.date <= pd.to_datetime(new)) &
+        (data.country == select2.value)
+    , :].reset_index(drop=True)
+
+def x_axis_update_plot(attr, old, new):
+    ["Date", "Days since 100th case", "Days since 10th death"]
+    x_col = "date"
+    if new == "Date":
+        x_col = "date"
+        for p in all_plots:
+            p.xaxis.formatter=DatetimeTickFormatter(days=["%d %B %Y"],months=["%d %B %Y"],years=["%d %B %Y"])
+    elif new == "Days since 100th case":
+        x_col = "days_since_100"
+        for p in all_plots:
+            p.xaxis.formatter=NumeralTickFormatter(format="0,0")
+    elif new == "Days since 10th death":
+        x_col = "days_since_10"
+        for p in all_plots: 
+            p.xaxis.formatter=NumeralTickFormatter(format="0,0")
+    data.x_col = data[x_col]
+    source.data = data.loc[
+        (data.date >= pd.to_datetime(select3.value)) & 
+        (data.date <= pd.to_datetime(select4.value)) &
+        (data.country == select1.value)
+    , :].reset_index(drop=True)
+    source2.data = data.loc[
+        (data.date >= pd.to_datetime(select3.value)) & 
+        (data.date <= pd.to_datetime(select4.value)) &
+        (data.country == select2.value)
+    , :].reset_index(drop=True)
+
+
+# dropdowns
+select1 = Select(title="Country 1", options=countries, value="World", css_classes=["country_1"])
 select1.on_change("value", country_1_update_plot)
 
-select2 = Select(title="Country 2", options=["Italy", "US"], value="US")
+select2 = Select(title="Country 2", options=countries, value="None", css_classes=["country_2"])
 select2.on_change("value", country_2_update_plot)
 
-p = figure(tools=["reset", "pan", "zoom_in", "zoom_out","save"])
-line_plot(source, p, 'blue', select1.value)
-line_plot(source2, p, 'orange', select2.value)
-p.add_tools(
-        HoverTool(tooltips=[('Country', '@country'), ('Confirmed Cases','@confirmed')])
-    )
+select3 = Select(title="Date Start", options=dates, value=dates[0])
+select3.on_change("value", date_start_update_plot)
 
-layout = column(row(select1, select2), p, name="line_plot")
+select4 = Select(title="Date End", options=dates, value=dates[len(dates)-1])
+select4.on_change("value", date_end_update_plot)
+
+x_col = Select(title="X Axis", options=["Date", "Days since 100th case", "Days since 10th death"], value="Date")
+x_col.on_change("value", x_axis_update_plot)
+
+
+# plots
+confirmed = add_plot(line_plot, 'confirmed', 'Confirmed Cases')
+deaths = add_plot(line_plot, 'deaths', 'Deaths')
+new_cases = add_plot(line_plot, 'new_cases', 'New Cases')
+new_deaths = add_plot(line_plot, 'new_deaths', 'New Deaths')
+case_accel = add_plot(line_plot, 'acceleration_cases', 'Cases Acceleration')
+death_accel = add_plot(line_plot, 'acceleration_deaths', 'Deaths Acceleration')
+all_plots = [confirmed, deaths, new_cases, new_deaths, case_accel, death_accel]
+# initialize plots with date format
+for p in all_plots:
+    p.xaxis.formatter=DatetimeTickFormatter(days=["%d %B %Y"],months=["%d %B %Y"],years=["%d %B %Y"])
+
+
+# final layout
+layout = column(
+    row(select1, select2),
+    row(select3, select4, x_col), 
+    row(confirmed, deaths),
+    row(new_cases, new_deaths),
+    row(case_accel, death_accel)
+)
 curdoc().add_root(layout)
 curdoc().title = "COVID-19 Status Report"
